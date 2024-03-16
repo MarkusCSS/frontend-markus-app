7dc7a20390bfff4641c3df3245c6c5f10579dd08
Vračaričić Marko <marko.vracaricic.83@gmail.com>
2024-03-16 21:21:51 +0100

    Checking changed color of background

--- generateColorsAndBackgrounds.js
+++ generateColorsAndBackgrounds.js
@@ -2,7 +2,7 @@ export const colorsBackgrounds =[
     {
         "id":1,
         "logo":"assets/logo.png",
-        "backgroundUrl":"rgb(220, 220, 220)",
+        "backgroundUrl":"lightgoldenrodyellow",
         "color":"rgba(0,0,0,1)",
         "colorsOfContactQuestion":"rgb(0,0,0,.125)",
         "navbarBackgroundImg":""
